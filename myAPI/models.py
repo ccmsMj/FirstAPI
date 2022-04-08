6aee70d6aae278bf3deacf9c2646c4b19a193f47
ccmsMj <joshuavillaverde29@gmail.com>
2022-04-08 15:52:31 +0800

    created a new model called vehicls

--- myAPI/models.py
+++ myAPI/models.py
@@ -1,3 +1,15 @@
+from itertools import chain
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+
+class vehicls(models.Model):
+    name = models.CharField(max_length=60)
+    description = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
